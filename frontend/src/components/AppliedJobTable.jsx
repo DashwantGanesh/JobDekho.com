238a4fb191becff158dae16cbf036eec6cf019a0
Ganesh Dashwant <159054139+DashwantGanesh@users.noreply.github.com>
2025-04-15 20:50:53 +0530

    Update AppliedJobTable.jsx

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -4,34 +4,44 @@ import { Badge } from './ui/badge'
 import { useSelector } from 'react-redux'
 
 const AppliedJobTable = () => {
-    const {allAppliedJobs}=useSelector((store)=>store.job)
-  return (
-    <div>
-        <Table>
-            <TableCaption>List of your applied jobs.</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Date</TableHead>
-                    <TableHead>Job Role</TableHead>
-                    <TableHead>Company</TableHead>
-                    <TableHead className='text-right'>Status</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {
-                   allAppliedJobs.length === 0 ? <span>You haven't applied for any job.</span> :  allAppliedJobs.filter((appliedJob) => appliedJob?.job).map((appliedJob)=>(
-                        <TableRow key={appliedJob._id}>
-                            <TableCell>{appliedJob?.createdAt.split("T")[0]}</TableCell>
-                            <TableCell>{appliedJob.job.title || "Untitled"}</TableCell>
-                            <TableCell>{appliedJob.job.company.name || "Unknown"}</TableCell>
-                            <TableCell className='text-right'><Badge className={`${appliedJob?.status === "rejected" ? "bg-red-400" : appliedJob?.status === "pending" ? "bg-gray-500" : "bg-green-600" }`}>{appliedJob.status.toUpperCase() || "UNKNOWN"}</Badge></TableCell>
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
-        </Table>
-    </div>
-  )
+    const { allAppliedJobs } = useSelector((store) => store.job)
+    return (
+        <div>
+            <Table>
+                <TableCaption>List of your applied jobs.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Date</TableHead>
+                        <TableHead>Job Role</TableHead>
+                        <TableHead>Company</TableHead>
+                        <TableHead className='text-right'>Status</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        allAppliedJobs.length === 0 ? (
+                            <TableRow>
+                                <TableCell colSpan={4} className="text-center text-gray-500">
+                                    You haven't applied for any job.
+                                </TableCell>
+                            </TableRow>
+                        ) : allAppliedJobs.filter((appliedJob) => appliedJob?.job).map((appliedJob) => (
+                            <TableRow key={appliedJob._id}>
+                                <TableCell>{appliedJob?.createdAt.split("T")[0]}</TableCell>
+                                <TableCell>{appliedJob.job.title || "Untitled"}</TableCell>
+                                <TableCell>{appliedJob.job.company.name || "Unknown"}</TableCell>
+                                <TableCell className='text-right'>
+                                    <Badge className={`${appliedJob?.status === "rejected" ? "bg-red-400" : appliedJob?.status === "pending" ? "bg-gray-500" : "bg-green-600"}`}>
+                                        {appliedJob.status.toUpperCase() || "UNKNOWN"}
+                                    </Badge>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </div>
+    )
 }
 
 export default AppliedJobTable
